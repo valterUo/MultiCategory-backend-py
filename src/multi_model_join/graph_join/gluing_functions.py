481a605b2fe0043e19045cc0fcf3a1389598bee9
valterUo <valter.uotila@gmail.com>
2020-05-28 18:51:28 +0300

    Remove unnecessary shallow copies of graphs

--- src/multi_model_join/graph_join/gluing_functions.py
+++ src/multi_model_join/graph_join/gluing_functions.py
@@ -2,14 +2,13 @@ import networkx as nx
 from multi_model_join.graph_join.graph_join_error import GraphJoinError
 
 def glue_graphs(graph1, node0, graph2, node1, gluing_pattern):
-    copy_gluing_graph = gluing_pattern.copy()
     composition_graph = graph1
     if node0 not in graph1:
         raise GraphJoinError(node0, "Node 1 not in the graph 1!") 
     if node1 not in graph2:
         raise GraphJoinError(node1, "Node 1 not in the graph 1!")
     if gluing_pattern.number_of_nodes() > 1:
-        fst_pattern = replace_node(copy_gluing_graph, 0, node0)
+        fst_pattern = replace_node(gluing_pattern, 0, node0)
         sn_pattern = replace_node(fst_pattern, 1, node1)
         composition_graph = nx.compose_all([graph1, graph2, sn_pattern])
     elif gluing_pattern.number_of_nodes() == 0:
@@ -19,22 +18,17 @@ def glue_graphs(graph1, node0, graph2, node1, gluing_pattern):
     return composition_graph
 
 def replace_node(graph, old_node, new_node):
-    # print(new_node)
-    # print(old_node)
-    new_graph = graph.copy()
-    predecessors_of_old_node = new_graph.predecessors(old_node)
-    successors_of_old_node = new_graph.successors(old_node)
-    new_graph.remove_node(old_node)
-    new_graph.add_node(new_node)
+    predecessors_of_old_node = graph.predecessors(old_node)
+    successors_of_old_node = graph.successors(old_node)
+    graph.remove_node(old_node)
+    graph.add_node(new_node)
     for node in predecessors_of_old_node:
-        new_graph.add_edge(node, new_node)
+        graph.add_edge(node, new_node)
     for node in successors_of_old_node:
-        new_graph.add_edge(new_node, node)
-    return new_graph
+        graph.add_edge(new_node, node)
+    return graph
 
 def overlay_on_single_node(graph1, node0, graph2, node1):
-    # print("Node 1:", node0)
-    # print("Node 2:", node1)
     new_node = frozenset({node0, node1})
     if len(new_node) == 1:
         new_node = list(new_node)[0]
